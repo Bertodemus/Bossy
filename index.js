146480234037b80c4d2a71d757847909d5902d12
Bertodemus <roberto@robertorubet.com>
2020-11-07 23:56:34 -0500

    adding in structure to the files

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require("express");
 var mysql = require("mysql");
+var addin = require('./lib/add');
+var remout = require('./lib/remove');
+var upd = require('./lib/update')
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -20,6 +23,71 @@ connection.connect(function(err) {
     console.error("error connecting: " + err.stack);
     return;
   }
-
   console.log("connected as id " + connection.threadId);
+  runCMS();
 });
+
+function runCMS() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View Departments",
+                "View Roles",
+                "View Employees",
+                "Add Department",
+                "Add Role",
+                "Add Employee",
+                "Update Employee Roles",
+                "exit"
+            ]
+        })
+        .then(function(answer) {
+            switch (answer.action) {
+            case "View Departments":
+                addin.viewD();
+                break;
+    
+            case "View Roles":
+                viewR();
+                break;
+    
+            case "View Employees":
+                viewE();
+                break;
+
+            case "Add Department":
+                addD();
+                break;
+
+            case "Add Role":
+                addR();
+                break;
+
+            case "Add Employee":
+                addE();
+                break;
+
+            case "Update Employee Role":
+                updateEmpRole();
+                break;
+    
+            case "exit":
+                connection.end();
+                break;
+            }
+        });
+    }
+
+
+
+
+
+  // Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+
